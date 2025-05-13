716b863681e2f2b0e7cac31819b6320e47b71038
venkat20000612 <venkataraoannam97@gmail.com>
2025-05-13 14:18:41 +0530

    update the remuse file

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -20,6 +20,7 @@ const Contact = () => {
         Message: "",
     });
 
+    
     const handleChange = (e) => {
         const { name, value } = e.target;
         setFormData({ ...formData, [name]: value });
